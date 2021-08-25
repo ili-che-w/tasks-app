428ce6fa62f4abdd011de7604d75c81f9c378592
Ильичёв Иван Сергеевич <iilichev@corp.local>
2021-08-26 00:34:21 +0300

    [+] create task title

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    <h1>Create</h1>
+  <div class="row">
+    <div class="col s6 offset-s3">
+      <h1>Create task</h1>
+
+      <div class="input-field">
+        <input id="title" type="text" class="validate" required />
+        <label for="title">Title</label>
+      </div>
+    </div>
   </div>
 </template>
