da648762443153664f42f0fecd1c4eda04ba53fa
Ильичёв Иван Сергеевич <iilichev@corp.local>
2021-08-25 23:59:28 +0300

    [+] navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <div class="nav-wrapper">
+      <router-link to="/" class="brand-logo">Tasks</router-link>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li>
+          <router-link to="/" exact active-class="active">Create</router-link>
+        </li>
+
+        <li>
+          <router-link to="/list" exact active-class="active">List</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
