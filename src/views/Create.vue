7cd883e75c3d8c4ef392f2ac4e0bbd32239c0828
Ильичёв Иван Сергеевич <iilichev@corp.local>
2021-08-26 01:02:30 +0300

    [+] create task description

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -10,6 +10,19 @@
       </div>
 
       <div class="chips" ref="tags"></div>
+
+      <div class="input-field">
+        <textarea
+          id="description"
+          class="materialize-textarea"
+          v-model="description"
+          :maxlength="descriptionMaxLength"
+        ></textarea>
+        <label for="description">Description</label>
+        <span class="character-counter">
+          {{ description.length }}/{{ descriptionMaxLength }}
+        </span>
+      </div>
     </div>
   </div>
 </template>
@@ -18,6 +31,12 @@
 import M from 'materialize-css/dist/js/materialize.min'
 
 export default {
+  data() {
+    return {
+      description: '',
+      descriptionMaxLength: 2048
+    }
+  },
   mounted() {
     M.Chips.init(this.$refs.tags, {
       placeholder: 'Task tags'
@@ -25,3 +44,10 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.character-counter {
+  float: right;
+  font-size: 12px;
+}
+</style>
